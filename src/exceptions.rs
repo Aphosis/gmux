cf79f2f3f5c1cf05437b91f06cd1bb3fdeb16f05
Aphosis <aphosis.github@gmail.com>
2020-10-26 17:53:50 +0100

    ✨ Defined custom errors of the CLI

---
+++ src/exceptions.rs
@@ -0,0 +1,33 @@
+use std::fmt;
+use std::path::PathBuf;
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    PoolAlreadyExists { label: String },
+    PoolDoesNotExists { label: String },
+    RepositoryDoesNotExists { path: PathBuf },
+    FileDoesNotExists { path: PathBuf },
+    InvalidSettingsFile,
+    NoCurrentPoolSet,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::PoolAlreadyExists { label } => write!(f, "Pool '{}' already exists.", &label),
+            Error::PoolDoesNotExists { label } => write!(f, "Pool '{}' does not exists.", &label),
+            Error::RepositoryDoesNotExists { path } => write!(f, "Repository '{}' does not exists.", &path.display()),
+            Error::FileDoesNotExists { path } => write!(f, "File '{}' does not exists.", &path.display()),
+            Error::InvalidSettingsFile => {
+                write!(f, "Settings file is invalid, do you have a home folder ?")
+            }
+            Error::NoCurrentPoolSet => {
+                write!(f, "No pool is currently set, create one using `rit pool new` or set an existing one with `rit pool set`.")
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
 mod app;
+mod exceptions;
 pub use self::app::*;
+pub use self::exceptions::*;
