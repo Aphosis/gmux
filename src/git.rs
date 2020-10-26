c2458a2dd02d764c689f66a53cb0d9ddf8ad9989
Aphosis <aphosis.github@gmail.com>
2020-10-26 18:55:00 +0100

    ✨ Separate `rit command` repositories output
    Git commands output are now decorated with the repository name and path,
    relative to the pool root.
    
    In addition, decoration is colored blue for messages and red for errors.

--- src/git.rs
+++ src/git.rs
@@ -1,4 +1,6 @@
 use super::{Pool, Result, Settings};
+use colored::*;
+use std::io::prelude::*;
 use std::path::PathBuf;
 use std::process::Command;
 
@@ -24,13 +26,23 @@ impl GitCaller {
             let mut repository_args = vec!["-C".into(), path.into()];
             repository_args.append(&mut args.clone());
 
-            let status = Command::new(&self.executable)
+            let output = Command::new(&self.executable)
                 .args(repository_args)
-                .status()?;
+                .output()?;
 
-            if !status.success() {
-                return Err("Process failed.".into());
-            }
+            let header = format!("- {}\n", repository.path.join(&repository.name).display());
+
+            if !output.stderr.is_empty() {
+                let header = header.red().bytes().collect::<Vec<u8>>();
+                std::io::stdout().write_all(&header)?;
+                std::io::stdout().write_all(&output.stderr)?;
+            } else if !output.stdout.is_empty() {
+                let header = header.blue().bytes().collect::<Vec<u8>>();
+                std::io::stdout().write_all(&header)?;
+                std::io::stdout().write_all(&output.stdout)?;
+            } else {
+                continue;
+            };
         }
         Ok(())
     }
