12815d11780af7ba522e7aa6cd1f451f332c2d49
Aphosis <aphosis.github@gmail.com>
2022-01-12 23:16:39 +0100

    Display both the command output and error, if any
    `GitCaller` was also refactored to use a mpsc channel.
    
    Filtering is now done in a dedicated method, and is not
    done inside `rayon` jobs anymore.

--- src/git.rs
+++ src/git.rs
@@ -2,20 +2,16 @@ use super::{Pool, Result, Settings};
 use colored::*;
 use rayon::prelude::*;
 use regex::Regex;
+use std::fmt;
 use std::io::prelude::*;
 use std::path::PathBuf;
 use std::process::{Command, Output};
+use std::sync::mpsc::channel;
 
 #[derive(Debug)]
-enum LogLevel {
-    Info,
-    Error,
-}
-
-#[derive(Debug)]
-struct OutputLog {
-    level: LogLevel,
-    message: String,
+struct CommandOutput {
+    header: String,
+    output: std::io::Result<Output>,
 }
 
 pub struct GitCaller {
@@ -24,25 +20,29 @@ pub struct GitCaller {
 }
 
 impl GitCaller {
+
     pub fn new(settings: &Settings) -> Result<Self> {
         let executable = settings.executable.clone();
         let pool = Pool::from_current(&settings)?;
         Ok(GitCaller { executable, pool })
     }
+
     pub fn call(
         &self,
         args: Vec<String>,
         filter: Option<String>,
         exclude_filter: Option<String>,
     ) -> Result<()> {
-        let messages: Vec<Option<String>> = self
-            .pool
+        let (sender, receiver) = channel();
+
+        self.pool
             .repositories
             .par_iter()
-            .map(|repository| {
+            .for_each_with(sender, |s, repository| {
+
                 let path = repository.full_path(&self.pool.root);
                 let path = match path.to_str() {
-                    None => return None,
+                    None => return (),
                     Some(path) => path,
                 };
 
@@ -52,78 +52,115 @@ impl GitCaller {
                 let exe = self.executable.clone();
                 let header = format!("- {}\n", repository.path.join(&repository.name).display());
 
-                let output_result = Command::new(exe).args(repository_args).output();
+                let output = Command::new(exe).args(repository_args).output();
 
-                if let Some(output_log) = GitCaller::log_from_output_results(
-                    output_result,
-                    filter.clone(),
-                    exclude_filter.clone(),
-                ) {
-                    let colored_header = match output_log.level {
-                        LogLevel::Error => header.red(),
-                        LogLevel::Info => header.blue(),
-                    };
-                    let message = format!("{}\n{}\n", colored_header, output_log.message.trim());
-                    Some(message)
-                } else {
-                    None
+                s.send(CommandOutput { header, output })
+                    .expect("Cannot send git command output to channel receiver.");
+
+            });
+
+        for command_output in receiver {
+            match command_output.output {
+                Ok(output) => {
+                    let filtered_output = FilteredOutput::from(output, &filter, &exclude_filter);
+
+                    if filtered_output.is_empty() {
+                        continue;
+                    }
+
+                    let message = format!("{}\n{}", command_output.header.blue(), filtered_output);
+
+                    std::io::stdout()
+                        .write_all(&message.as_bytes())
+                        .expect("Could not write command output to stdout.");
                 }
-            })
-            .collect();
-
-        for message in messages {
-            if let Some(msg) = message {
-                std::io::stdout()
-                    .write_all(&msg.as_bytes())
-                    .expect("Could not write command output to stdout.");
+                Err(_) => (),
             }
         }
-
         Ok(())
     }
-    fn log_from_output_results(
-        output_results: std::io::Result<Output>,
-        filter: Option<String>,
-        exclude_filter: Option<String>,
-    ) -> Option<OutputLog> {
-        match output_results {
-            Ok(output) => {
-                if !output.stderr.is_empty() {
-                    Some(OutputLog {
-                        message: String::from_utf8(output.stderr)
-                            .expect("Could not decode error message"),
-                        level: LogLevel::Error,
-                    })
-                } else if !output.stdout.is_empty() {
-                    if let Some(ref pattern) = exclude_filter {
-                        let re = Regex::new(&pattern).ok()?;
-                        let out = std::str::from_utf8(&output.stdout).ok()?;
-                        if re.is_match(out) {
-                            return None;
-                        }
-                    }
+}
 
-                    if let Some(ref pattern) = filter {
-                        let re = Regex::new(&pattern).ok()?;
-                        let out = std::str::from_utf8(&output.stdout).ok()?;
-                        if !re.is_match(out) {
-                            return None;
-                        }
-                    }
+#[derive(Debug)]
+struct FilteredOutput {
+    out: Option<String>,
+    err: Option<String>,
+}
 
-                    Some(OutputLog {
-                        message: String::from_utf8(output.stdout)
-                            .expect("Could not decode git message"),
-                        level: LogLevel::Info,
-                    })
-                } else {
+impl FilteredOutput {
+    fn from(output: Output, filter: &Option<String>, exclude: &Option<String>) -> Self {
+        let out = match FilteredOutput::bytes_to_string(&output.stdout) {
+            Some(message) => FilteredOutput::filter(message, filter, exclude),
+            None => None,
+        };
+        let err = match FilteredOutput::bytes_to_string(&output.stderr) {
+            Some(message) => FilteredOutput::filter(message, filter, exclude),
+            None => None,
+        };
+        FilteredOutput { out, err }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.out.is_none() && self.err.is_none()
+    }
+
+    fn bytes_to_string(bytes: &Vec<u8>) -> Option<String> {
+        if bytes.is_empty() {
+            return None;
+        }
+        match std::str::from_utf8(bytes) {
+            Ok(string) => Some(String::from(string)),
+            Err(_) => None,
+        }
+    }
+
+    fn filter(
+        message: String,
+        filter: &Option<String>,
+        exclude: &Option<String>,
+    ) -> Option<String> {
+        match exclude {
+            Some(pattern) => {
+                if FilteredOutput::is_match(&message, &pattern) {
                     return None;
                 }
             }
-            Err(err) => Some(OutputLog {
-                message: err.to_string(),
-                level: LogLevel::Error,
-            }),
+            None => (),
         }
+        match filter {
+            Some(pattern) => {
+                if !FilteredOutput::is_match(&message, &pattern) {
+                    return None;
+                }
+            }
+            None => (),
+        }
+        Some(message)
+    }
+
+    fn is_match(message: &String, pattern: &String) -> bool {
+        let re =
+            Regex::new(pattern).expect("Could not convert match pattern to regular expression.");
+        re.is_match(message)
+    }
+}
+
+impl fmt::Display for FilteredOutput {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.is_empty() {
+            return write!(f, "");
+        }
+
+        let mut message = String::from("");
+
+        if let Some(out) = &self.out {
+            message.push_str(&format!("{}\n", out.trim()));
+        }
+
+        if let Some(err) = &self.err {
+            message.push_str(&format!("{}\n", err.red().trim()));
+        }
+
+        write!(f, "{}", message)
     }
 }
